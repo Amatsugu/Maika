b930c34138b6075a61351446709e3eb6c1bfe8fe
Yu Lin <admin@yulin12345.site>
2017-11-30 01:20:47 -0500

    Update queries, removed password and changed type to is_public.

--- SQL and MODELS/Queries.sql
+++ SQL and MODELS/Queries.sql
@@ -4,8 +4,8 @@ SELECT * FROM project.user WHERE username = 'test' AND password = 'test';
 -- Retrieve fname, mname, lname, email, age if username and password match from user table --
 SELECT (email) FROM project.user WHERE username = 'test' AND password = 'test';
 
--- Retrieve user.user_id, room.room_id, room.title, room.password, room.type --
-SELECT u.user_id, r.room_id, r.title, r.password, r.type
+-- Retrieve user.user_id, room.room_id, room.title, room.is_public --
+SELECT u.user_id, r.room_id, r.title, r.is_public
 FROM project.user u
 LEFT JOIN project.room r ON u.user_id = r.owner_id;
 
@@ -22,14 +22,11 @@ WHERE room_id = 'e7d74670-d54d-11e7-9296-cec278b6b50a';
 -- Update Room title --
 UPDATE project.room SET title = 'test title' WHERE room_id = 'e7d74670-d54d-11e7-9296-cec278b6b50a';
 
--- Update Room password --
-UPDATE project.room SET password = '1234567' WHERE room_id = 'e7d74670-d54d-11e7-9296-cec278b6b50a';
-
 -- Update Room type --
 UPDATE project.room SET type = 'false' WHERE room_id = 'e7d74670-d54d-11e7-9296-cec278b6b50a';
 
--- Update Room number, password, and type --
-UPDATE project.room SET password = '12345', type = 'false' WHERE room_id = 'e7d74670-d54d-11e7-9296-cec278b6b50a';
+-- Update is_public by room_id --
+UPDATE project.room SET type = 'false' WHERE room_id = 'e7d74670-d54d-11e7-9296-cec278b6b50a';
 
 
 
@@ -39,7 +36,7 @@ UPDATE project.room SET password = '12345', type = 'false' WHERE room_id = 'e7d7
 INSERT INTO project.user (username, password, email) VALUES ('test1', 'test1', 'test@test.com');
 
 -- Insert Room --
-INSERT INTO project.room (owner_id, title, password, type) VALUES ('c492f646-d54d-11e7-9296-cec278b6b50a', 'test title', '123', 'false');
+INSERT INTO project.room (owner_id, title, type) VALUES ('c492f646-d54d-11e7-9296-cec278b6b50a', 'test title', 'false');
 
 -- Insert Room_Member --
 INSERT INTO project.room_member (room_id, user_id) VALUES ('e7d74670-d54d-11e7-9296-cec278b6b50a', 'c492f646-d54d-11e7-9296-cec278b6b50a');
@@ -88,6 +85,6 @@ INSERT INTO project.invite VALUES ('2da7ebfa-d54e-11e7-9296-cec278b6b50a', 'e7d7
 DELETE FROM project.invite WHERE invite_id = '2da7ebfa-d54e-11e7-9296-cec278b6b50a';
 
 --- Getting room info from invite link --
-SELECT DISTINCT r.title, r.password, r.type
+SELECT DISTINCT r.title, r.type
 FROM project.room r
 LEFT JOIN project.invite i ON r.room_id = i.room_id;
