8315533e4724bd0a0513268ebed362a65c2401dc
Khamraj Rohit <khamraj@luminousVector.com>
2017-12-02 14:30:27 -0500

    Refactoring

--- ITEC305-Project/Maika.cs
+++ ITEC305-Project/Maika.cs
@@ -31,17 +31,11 @@ namespace ITEC305_Project
 			}
 		});
 
-		private static NpgsqlConnection GetConnection()
-		{
-			var conn = new NpgsqlConnection(dBCredentials.ConntectionString);
-			conn.Open();
-			return conn;
-		}
-
 		private static T RunCommand<T>(Func<NpgsqlCommand, T> func)
 		{
-			using (var con = GetConnection())
+			using (var con = new NpgsqlConnection(dBCredentials.ConntectionString))
 			{
+				con.Open();
 				using (var cmd = con.CreateCommand())
 				{
 					return func(cmd);
@@ -49,210 +43,154 @@ namespace ITEC305_Project
 			}
 		}
 
-		internal static string ValidateUser(UserCredentialsModel login)
-		{
-			using (var con = GetConnection())
+		internal static string ValidateUser(UserCredentialsModel login) =>
+			RunCommand( cmd =>
 			{
-				using (var cmd = con.CreateCommand())
+				cmd.CommandText = $"SELECT password, user_id FROM users WHERE email = '{Uri.EscapeDataString(login.Email)}'";
+				using (var reader = cmd.ExecuteReader())
 				{
-					cmd.CommandText = $"SELECT password, user_id FROM project.user WHERE email = '{Uri.EscapeDataString(login.Email)}'";
-					using (var reader = cmd.ExecuteReader())
-					{
-						reader.Read();
-						if (Utils.VerifyPassword(login.Password, reader.GetString(0)))
-							return Authenticator.Authenticate(new UserPrincipal(reader.GetString(1), login.Username));
-						else
-							return null;
-					}
+					reader.Read();
+					if (Utils.VerifyPassword(login.Password, reader.GetString(0)))
+						return Authenticator.Authenticate(new UserPrincipal(reader.GetString(1), login.Username));
+					else
+						return null;
 				}
-			}
-		}
+			});
 
-		internal static UserModel CreateUser(UserCredentialsModel user)
-		{
-			using (var con = GetConnection())
+		internal static UserModel CreateUser(UserCredentialsModel user) =>
+			RunCommand(cmd =>
 			{
-				using (var cmd = con.CreateCommand())
+				var id = Authenticator.GenerateToken();
+				cmd.CommandText = $"INSERT INTO users (user_id, username, password, email) VALUES ('{id}', '{Uri.EscapeDataString(user.Username)}', '{Utils.HashPassword(user.Password)}', '{Uri.EscapeDataString(user.Email)}');";
+				cmd.ExecuteNonQuery();
+				return new UserModel
 				{
-					var id = Authenticator.GenerateToken();
-					cmd.CommandText = $"INSERT INTO project.user (user_id, username, password, email) VALUES ('{id}', '{Uri.EscapeDataString(user.Username)}', '{Utils.HashPassword(user.Password)}', '{Uri.EscapeDataString(user.Email)}');";
-					cmd.ExecuteNonQuery();
+					Username = user.Username,
+					Id = id
+				};
+			});
+
+		internal static UserModel GetUser(string userId) =>
+			RunCommand(cmd =>
+			{
+				cmd.CommandText = $"SELECT username FROM users WHERE user_id = '{userId}'";
+				using (var reader = cmd.ExecuteReader())
+				{
+					if (!reader.HasRows)
+						return null;
+					reader.Read();
 					return new UserModel
 					{
-						Username = user.Username,
-						Id = id
+						Id = userId,
+						Username = Uri.UnescapeDataString(reader.GetString(0))
 					};
 				}
-			}
-		}
+			});
 
-		internal static UserModel GetUser(string userId)
-		{
-			using (var con = GetConnection())
+		internal static bool CheckEmailExists(string email) =>
+			RunCommand(cmd =>
 			{
-				using (var cmd = con.CreateCommand())
-				{
-					cmd.CommandText = $"SELECT username FROM project.user WHERE user_id = '{userId}'";
-					using (var reader = cmd.ExecuteReader())
-					{
-						if (!reader.HasRows)
-							return null;
-						reader.Read();
-						return new UserModel
-						{
-							Id = userId,
-							Username = Uri.UnescapeDataString(reader.GetString(0))
-						};
-					}
-				}
-			}
-		}
-
-		internal static bool CheckEmailExists(string email)
-		{
-			using (var con = GetConnection())
-			{
-				using (var cmd = con.CreateCommand())
-				{
-					cmd.CommandText = $"COUNT (SELECT username FROM project.user WHERE email = '{email}')";
-					return ((int)cmd.ExecuteScalar()) > 0;
-				}
-			}
-		}
+				cmd.CommandText = $"COUNT (SELECT username FROM users WHERE email = '{email}')";
+				return ((int)cmd.ExecuteScalar()) > 0;
+			});
 
 		internal static string CreateInvite() //TODO: Create Invite
 		{
 			throw new NotImplementedException();
 		}
 
-		internal static bool SetUsername(string userid, UserCredentialsModel user)
-		{
-			using (var con = GetConnection())
+		internal static bool SetUsername(string userid, UserCredentialsModel user) =>
+			RunCommand(cmd =>
 			{
-				using (var cmd = con.CreateCommand())
-				{
-					cmd.CommandText = $"UPDATE project.user SET username = '{Uri.EscapeDataString(user.Username)}' WHERE user_id = '{userid}'";
-					return cmd.ExecuteNonQuery() > 0;
-				}
-			}
-		}
+				cmd.CommandText = $"UPDATE users SET username = '{Uri.EscapeDataString(user.Username)}' WHERE user_id = '{userid}'";
+				return cmd.ExecuteNonQuery() > 0;
+			});
 
 		internal static RoomModel CreateRoom(string ownerId) //TODO: Create Room
 		{
 			throw new NotImplementedException();
 		}
 
-		internal static RoomModel GetRoomInfo(string roomId)
-		{
-			using (var con = GetConnection())
+		internal static RoomModel GetRoomInfo(string roomId) =>
+			RunCommand(cmd =>
 			{
-				using (var cmd = con.CreateCommand())
+				cmd.CommandText = $"SELECT title, owner_id FROM room WHERE room_id = '{roomId}'";
+				using (var reader = cmd.ExecuteReader())
 				{
-					cmd.CommandText = $"SELECT title, owner_id FROM project.room WHERE room_id = '{roomId}'";
-					using (var reader = cmd.ExecuteReader())
+					if (!reader.HasRows)
+						return null;
+					reader.Read();
+					return new RoomModel
 					{
-						if (!reader.HasRows)
-							return null;
-						reader.Read();
-						return new RoomModel
-						{
-							Id = roomId,
-							Name = Uri.UnescapeDataString(reader.GetString(0)),
-							Owner = GetUser(reader.GetString(1)),
-							Members = GetRoomMembers(roomId)
-						};
-					}
+						Id = roomId,
+						Name = Uri.UnescapeDataString(reader.GetString(0)),
+						Owner = GetUser(reader.GetString(1)),
+						Members = GetRoomMembers(roomId)
+					};
 				}
-			}
-		}
+			});
 
 		internal static bool JoinRoom(string roomID, string userId) => 
 			RunCommand(cmd =>
 			{
-				cmd.CommandText = $"INSERT INTO project.room_member (room_id, user_id) VALUES ({roomID}, {userId})";
+				cmd.CommandText = $"INSERT INTO room_member (room_id, user_id) VALUES ({roomID}, {userId})";
 				return cmd.ExecuteNonQuery() > 0;
 			});
 
 		internal static bool LeaveRoom(string userId) =>
 			RunCommand(cmd =>
 			{
-				cmd.CommandText = $"DELETE FROM project.room_member WHERE user_id = '{userId}'";
+				cmd.CommandText = $"DELETE FROM room_member WHERE user_id = '{userId}'";
 				return cmd.ExecuteNonQuery() > 0;
 			});
 
-		internal static List<UserModel> GetRoomMembers(string roomId)
-		{
-			using (var con = GetConnection())
+		internal static List<UserModel> GetRoomMembers(string roomId) =>
+			RunCommand(cmd =>
 			{
-				using (var cmd = con.CreateCommand())
+				cmd.CommandText = $"SELECT u.user_id, u.username FROM user u LEFT JOIN room_member rm ON u.user_id = rm.user_id WHERE room_id = '{roomId }'";
+				using (var reader = cmd.ExecuteReader())
 				{
-					cmd.CommandText = $"SELECT u.user_id, u.username FROM project.user u LEFT JOIN project.room_member rm ON u.user_id = rm.user_id WHERE room_id = '{roomId }'";
-					using (var reader = cmd.ExecuteReader())
+					if (!reader.HasRows)
+						return null;
+					List<UserModel> users = new List<UserModel>();
+					while (reader.Read())
 					{
-						if (!reader.HasRows)
-							return null;
-						List<UserModel> users = new List<UserModel>();
-						while (reader.Read())
+						users.Add(new UserModel
 						{
-							users.Add(new UserModel
-							{
-								Id = reader.GetString(0),
-								Username = Uri.UnescapeDataString(reader.GetString(1))
-							});
-						}
-						return users;
+							Id = reader.GetString(0),
+							Username = Uri.UnescapeDataString(reader.GetString(1))
+						});
 					}
+					return users;
 				}
-			}
-		}
+			});
 
-		internal static bool SetRoomName(string roomId, string userId, string roomName)
-		{
-			using (var con = GetConnection())
+		internal static bool SetRoomName(string roomId, string userId, string roomName) =>
+			RunCommand(cmd =>
 			{
-				using (var cmd = con.CreateCommand())
-				{
-					cmd.CommandText = $"UPDATE project.room SET title = '{Uri.EscapeDataString(roomName)}' WHERE room_id = '{roomId}' AND owner_id = '{userId}'";
-					return cmd.ExecuteNonQuery() > 0;
-				}
-			}
-		}
+				cmd.CommandText = $"UPDATE room SET title = '{Uri.EscapeDataString(roomName)}' WHERE room_id = '{roomId}' AND owner_id = '{userId}'";
+				return cmd.ExecuteNonQuery() > 0;
+			});
 
-		internal static bool CloseRoom(string roomId, string userId)
-		{
-			using (var con = GetConnection())
+		internal static bool CloseRoom(string roomId, string userId) =>
+			RunCommand(cmd =>
 			{
-				using (var cmd = con.CreateCommand())
-				{
-					cmd.CommandText = $"DELETE FROM project.room WHERE room_id = '{roomId}' AND owner_id = '{userId}';";
-					cmd.CommandText += $"DELETE FROM project.room_member WHERE room_id = '{roomId}';";
-					return cmd.ExecuteNonQuery() > 0;
-				}
-			}
-		}
+				cmd.CommandText = $"DELETE FROM room WHERE room_id = '{roomId}' AND owner_id = '{userId}';";
+				return cmd.ExecuteNonQuery() > 0;
+			});
 
-		internal static bool SetOwner(string roomId, string userId, string newOwnerId)
-		{
-			using (var con = GetConnection())
+		internal static bool SetOwner(string roomId, string userId, string newOwnerId) =>
+			RunCommand(cmd =>
 			{
-				using (var cmd = con.CreateCommand())
-				{
-					cmd.CommandText = $"UPDATE project.room SET user_id = '{newOwnerId}' WHERE room_id = '{roomId}' AND user_id = '{userId}'";
-					return cmd.ExecuteNonQuery() > 0;
-				}
-			}
-		}
+				cmd.CommandText = $"UPDATE room SET user_id = '{newOwnerId}' WHERE room_id = '{roomId}' AND user_id = '{userId}'";
+				return cmd.ExecuteNonQuery() > 0;
+			});
 
-		internal static bool DeleteInvite(string inviteId)
-		{
-			using (var con = GetConnection())
+		internal static bool DeleteInvite(string inviteId) =>
+			RunCommand(cmd =>
 			{
-				using (var cmd = con.CreateCommand())
-				{
-					cmd.CommandText = $"DELETE FROM project.invite WHERE invite_id = '{inviteId}'";
-					return cmd.ExecuteNonQuery() > 0;
-				}
-			}
-		}
+				cmd.CommandText = $"DELETE FROM invite WHERE invite_id = '{inviteId}'";
+				return cmd.ExecuteNonQuery() > 0;
+			});
 	}
 }
--- SQL and MODELS/SQL Tables.sql
+++ SQL and MODELS/SQL Tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "project"."user"
+CREATE TABLE "user"
   (
      "user_id"  VARCHAR(50) NOT NULL,
      "username" VARCHAR(30) NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE "project"."user"
      PRIMARY KEY ("user_id")
   ) without oids;
   
-CREATE TABLE "project"."room"
+CREATE TABLE "room"
   (
      "room_id"  VARCHAR(50) NOT NULL,
      "owner_id"  VARCHAR(50) NOT NULL,
@@ -16,54 +16,54 @@ CREATE TABLE "project"."room"
      PRIMARY KEY ("room_id")
   ) without oids;
   
-CREATE TABLE "project"."room_member"
+CREATE TABLE "room_member"
   (
      "room_id"        VARCHAR(50) NOT NULL,
      "user_id"        VARCHAR(50) NOT NULL
   ) without oids;
   
-CREATE TABLE "project"."invite"
+CREATE TABLE "invite"
   (
      "invite_id"        VARCHAR(50) NOT NULL,
      "room_id"        VARCHAR(50) NOT NULL,
 	 PRIMARY KEY ("invite_id")
   ) without oids;
   
-ALTER TABLE "project"."room"
+ALTER TABLE "room"
   ADD CONSTRAINT "user_id_FK_user" FOREIGN KEY ("owner_id") REFERENCES
-  "project"."user" ("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
+  "user" ("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
   
-ALTER TABLE "project"."room_member"
+ALTER TABLE "room_member"
   ADD CONSTRAINT "room_id_FK-room" FOREIGN KEY ("room_id") REFERENCES
-  "project"."room" ("room_id") ON DELETE CASCADE ON UPDATE CASCADE;
+  "room" ("room_id") ON DELETE CASCADE ON UPDATE CASCADE;
   
-ALTER TABLE "project"."room_member"
+ALTER TABLE "room_member"
   ADD CONSTRAINT "user_id_FK-user" FOREIGN KEY ("user_id") REFERENCES
-  "project"."user" ("user_id") ON DELETE CASCADE ON UPDATE CASCADE; 
+  "user" ("user_id") ON DELETE CASCADE ON UPDATE CASCADE; 
   
-ALTER TABLE "project"."invite"
+ALTER TABLE "invite"
   ADD CONSTRAINT "room_id_FK-room" FOREIGN KEY ("room_id") REFERENCES
-  "project"."room" ("room_id") ON DELETE CASCADE ON UPDATE CASCADE; 
+  "room" ("room_id") ON DELETE CASCADE ON UPDATE CASCADE; 
   
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO "project"."user" VALUES ('c492f646-d54d-11e7-9296-cec278b6b50a', 'test', 'test', 'test@test.com');
-INSERT INTO "project"."user" VALUES ('c492fa74-d54d-11e7-9296-cec278b6b50a', 'test123', 'test123', 'test@test.com');
+INSERT INTO "user" VALUES ('c492f646-d54d-11e7-9296-cec278b6b50a', 'test', 'test', 'test@test.com');
+INSERT INTO "user" VALUES ('c492fa74-d54d-11e7-9296-cec278b6b50a', 'test123', 'test123', 'test@test.com');
 
 -- ----------------------------
 -- Records of room
 -- ----------------------------
-INSERT INTO "project"."room" VALUES ('e7d74670-d54d-11e7-9296-cec278b6b50a', 'c492f646-d54d-11e7-9296-cec278b6b50a', 'title', 'false');
+INSERT INTO "room" VALUES ('e7d74670-d54d-11e7-9296-cec278b6b50a', 'c492f646-d54d-11e7-9296-cec278b6b50a', 'title', 'false');
 
 -- ----------------------------
 -- Records of room_member
 -- ----------------------------
-INSERT INTO "project"."room_member" VALUES ('e7d74670-d54d-11e7-9296-cec278b6b50a', 'c492f646-d54d-11e7-9296-cec278b6b50a');
-INSERT INTO "project"."room_member" VALUES ('e7d74670-d54d-11e7-9296-cec278b6b50a', 'c492fa74-d54d-11e7-9296-cec278b6b50a');
+INSERT INTO "room_member" VALUES ('e7d74670-d54d-11e7-9296-cec278b6b50a', 'c492f646-d54d-11e7-9296-cec278b6b50a');
+INSERT INTO "room_member" VALUES ('e7d74670-d54d-11e7-9296-cec278b6b50a', 'c492fa74-d54d-11e7-9296-cec278b6b50a');
 
 -- ----------------------------
 -- Records of invite
 -- ----------------------------
-INSERT INTO "project"."invite" VALUES ('2da7ebfa-d54e-11e7-9296-cec278b6b50a', 'e7d74670-d54d-11e7-9296-cec278b6b50a');
-INSERT INTO "project"."invite" VALUES ('2da7ef42-d54e-11e7-9296-cec278b6b50a', 'e7d74670-d54d-11e7-9296-cec278b6b50a');
+INSERT INTO "invite" VALUES ('2da7ebfa-d54e-11e7-9296-cec278b6b50a', 'e7d74670-d54d-11e7-9296-cec278b6b50a');
+INSERT INTO "invite" VALUES ('2da7ef42-d54e-11e7-9296-cec278b6b50a', 'e7d74670-d54d-11e7-9296-cec278b6b50a');
