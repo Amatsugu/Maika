8151d7e3085980afc8c33262eb5e5d7c8f0fc14b
Khamraj Rohit <khamraj@luminousvector.com>
2017-12-03 16:58:37 -0500

    Prepare for Networking

--- canvasApp/js/canvasMod.js
+++ canvasApp/js/canvasMod.js
@@ -6,18 +6,31 @@
 var flag = false, dFlag = false;
 var x1, x2, y1, y2;
 var canvas;
+var thickness = 5;
+
+var caputeredEvents = [];
+var nextCapture;
+var interval = 1000*(20/60)
 
 $(document).ready(function() //TODO: Capture states for network transmition
 {
+	nextCapture = new Date().getTime() + interval;
+	setTimeout(function()
+	{
+		//TODO Send data
+		
+		nextCapture = new Date().getTime() + interval;
+		captureEvents = [];
+	}, interval);
 	canvas = $("canvas");
-	var context = $canvas[0].getContext("2d");
-	var xScale = 1920/canvas.offsetWidth, yScale = 1080/canvas.offsetHeigh; //TODO: Caclucate scale factor	
+	var context = canvas[0].getContext("2d");
+	var xScale = canvas.attr("width")/canvas.outerWidth(), yScale = canvas.attr("height")/canvas.outerHeight(); //TODO: Caclucate scale factor	
 	//On mouse events on the canvas
-	$canvas.mousedown(function(e){
+	canvas.mousedown(function(e){
 		x1 = x2;
 		y1 = y2;
-		x2 = e.clientX - canvas.offsetLeft;
-		y2 = e.clientY - canvas.offsetTop;
+		x2 = e.clientX - canvas.offset().left;
+		y2 = e.clientY - canvas.offset().top;
 
 		flag = true;
 		dFlag = true;
@@ -25,29 +38,58 @@ $(document).ready(function() //TODO: Capture states for network transmition
 		{
 			context.beginPath();
 			context.fillStyle = color;
-			context.fillRect((x2 + window.scrollX) * xScale, (y2 + window.scrollY) * yScale, 2, 2);
+			context.arc((x2 + window.scrollX) * xScale, (y2 + window.scrollY) * yScale, thickness/2, 0, 2 * Math.PI);
+			context.fill();
+			//context.fillRect((x2 + window.scrollX) * xScale, (y2 + window.scrollY) * yScale, thickness, thickness);
 			context.closePath();
 			dFlag = false;
+			captureEvents[captureEvents.length] = 
+			{
+				tpye:"dot",
+				p:
+				{
+					x:x2,
+					y:y2
+				},
+				color:color,
+				size:thickness
+			};
 		}
 	}).mousemove(function(e){
-		
 		if (flag) 
 		{
 			x1 = x2;
 			y1 = y2;
-			x2 = e.clientX - canvas.offsetLeft;
-			y2 = e.clientY - canvas.offsetTop;
+			x2 = e.clientX - canvas.offset().left;
+			y2 = e.clientY - canvas.offset().top;
 			context.beginPath();
 			context.moveTo((x1 + window.scrollX) * xScale, (y1 + window.scrollY) * yScale);
 			context.lineTo((x2 + window.scrollX) * xScale, (y2 + window.scrollY) * yScale);
 			context.lineJoin = context.lineCap = 'round';
+			context.lineWidth = thickness;	
 			context.strokeStyle = color;
 			context.stroke();
 			context.closePath();
+			captureEvents[captureEvents.length] = 
+			{
+				tpye:"line",
+				p1:
+				{
+					x:x1,
+					y:y1
+				},
+				p2:
+				{
+					x:x2,
+					y:y2
+				},
+				color:color,
+				size:thickness
+			};
 		}
 	}).mouseup(function(){
 		flag = false;
 	}).mouseleave(function(){
-		$canvas.mouseup();
+		canvas.mouseup();
 	});
 });
